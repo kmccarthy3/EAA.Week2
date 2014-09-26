7a65ecb30db36d9cf5e455ecdfec31acf9b881a0
kmccarthy3 <kirstymcc2@gmail.com>
2014-09-26 11:26:30 +0100

    Creating two subdirectories called bin and scripts in the folder EAA.Week2

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+#: Title: Setup
+#: Date: 26/09/2014
+#: Author: Kirsty McCarthy
+#: Version: 1
+#: Description: Creating a script that adds two subdirectories in the folder EAA.Week2.
+#: Options: 
+mkdir bin
+mkdir scripts
